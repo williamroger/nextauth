23496ded1be0b27356cad2d28ea1901aa455da61
William Roger <williamroger.frontend@gmail.com>
2022-06-17 20:51:12 -0300

    salvando tokens nos cookies

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../context/AuthContext";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-  console.log('user ', user);
+
   return (
     <h1>Dashboard: {user?.email}</h1>
   )
